3acf182ad177ad4c2b4c6a8a7eb934ab3249037a
unknown <Thorvald@AALBORGDATA.local>
2019-01-27 20:27:26 +0100

    Updated Service Worker

--- sw.js
+++ sw.js
@@ -1,16 +1,39 @@
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open('thorvald').then(function (cache) {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/styles.css',
-                '/images/Thorvald.jpg',
-                '/images/bullet.png',
-                '/images/bg-tile.png',
-                '/images/icons-192.png',
-                '/images/icons-512.png',
-            ]);
-        })
+self.addEventListener('install', function(event) {
+    console.log("install");
+    try {
+      console.log('typeof System in install', typeof System);
+    } catch (e) {}
+  
+    console.log('caching');
+    event.waitUntil(
+      caches.open('thorvald').then(function(cache) {
+        console.log('caching - getting');
+        return cache.addAll([
+            '/',
+            '/index.html',
+            '/styles.css',
+            '/images/Thorvald.jpg',
+            '/images/bullet.png',
+            '/images/bg-tile.png',
+            '/images/icons-192.png',
+            '/images/icons-512.png',
+    ]);
+      }).catch(function(error) {
+        console.log('error', error)
+      })
     );
-});
+  
+    self.addEventListener('fetch', function(event) {
+      console.log('fetching ->', event.request);
+      event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        })
+      );
+    });
+  });
