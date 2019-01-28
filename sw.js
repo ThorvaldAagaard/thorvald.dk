ed9660fcbda0903375622057cb7eadc39d9c7c2b
unknown <Thorvald@AALBORGDATA.local>
2019-01-28 14:09:54 +0100

    Removed console.log

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 self.addEventListener('install', function(event) {
     console.log("install");
     try {
-      console.log('typeof System in install', typeof System);
+      //console.log('typeof System in install', typeof System);
     } catch (e) {}
   
     console.log('caching');
     event.waitUntil(
       caches.open('thorvald').then(function(cache) {
-        console.log('caching - getting');
+        //console.log('caching - getting');
         return cache.addAll([
             '/',
             '/index.html',
@@ -24,7 +24,7 @@ self.addEventListener('install', function(event) {
     );
   
     self.addEventListener('fetch', function(event) {
-      console.log('fetching ->', event.request);
+      //console.log('fetching ->', event.request);
       event.respondWith(
         caches.match(event.request)
         .then(function(response) {
